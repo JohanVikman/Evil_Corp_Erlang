2aef6b061c8abace3ce10267ec3f6fc7cdf4673e
Johan Vikman <jowi7613+github@gmail.com>
2013-11-06 20:06:19 +0100

    Removed commented code

--- src/evil_censor.erl
+++ src/evil_censor.erl
@@ -25,20 +25,3 @@ replace_word_in_string(BlacklistWord, Tokens) ->
 		      end
 	      end,
 	      Tokens).
-	
-    
-    %% [
-    %%  begin 
-    %% 	 lists:map(fun(BlacklistWord ++ Suffix, BlacklistWord) ->
-    %% 			   lists:duplicate(length(BlacklistWord++Suffix), $X);
-    %% 		      (Token, BlacklistWord) ->
-    %% 			   Token
-    %% 		   end,
-    %% 		   Tokens) 
-    %%  end 
-    %%  || 
-    %% 	Token <- Tokens].
-
-
-%% CensorBar =  lists:duplicate(length(BlacklistWord),$X),
-%% re:replace(SentenceAcc, BlacklistWord, CensorBar, [global,{return,list}]).
